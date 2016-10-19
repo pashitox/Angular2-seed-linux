bfa80232985d709a289dd2938db848668df923d1
pashitox <pashitox@gmail.com>
2016-10-19 15:57:05 -0400

    mi primera modificacion

--- app/app.component.js
+++ app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular 2 App great day',
+            template: '<h1>My First Angular 2 App great day pashitox</h1>',
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { CourseComponent } from './course.component'
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App great day',
+  template: '<h1>My First Angular 2 App great day pashitox</h1>',
  
 })
 export class AppComponent { }
